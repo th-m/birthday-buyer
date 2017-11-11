986b774d47537cafb8931e06901ed1cfbfb8aa4e
thom <thom@MacBook-Air.local>
2017-11-11 00:09:42 -0700

    cron tab test

--- crons/buyGifts.js
+++ crons/buyGifts.js
@@ -31,8 +31,7 @@ firebaseAuth().signInWithEmailAndPassword("buyer@bot.com", "buystuff").then(func
   fetch('https://birthdaybuyer.herokuapp.com/').then(function(response) {
     return response.blob();
   }).then(function(myBlob) {
-    var objectURL = URL.createObjectURL(myBlob);
-    myImage.src = objectURL;
+   console.log(myBlob);
   });
   
 });
