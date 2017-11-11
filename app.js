235e4aa0d73498914622e9a86fc2316e115dbc7b
thom <thom@MacBook-Air.local>
2017-11-11 00:57:01 -0700

    added node fetch package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fetch = require('node-fetch');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
